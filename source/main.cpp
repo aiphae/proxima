f3818e0a8f28594e64ac47a1951845efa0eec7c8
aiphae <vl.diachenkoo@gmail.com>
2025-07-23 00:52:34 +0300

    Added application info

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,12 @@
 #include "main_window/main_window.h"
 #include <QApplication>
+#include <opencv2/core/version.hpp>
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
+    QCoreApplication::setOrganizationName("Proxima");
+    QCoreApplication::setApplicationName("Proxima");
 
     MainWindow w;
     w.show();
